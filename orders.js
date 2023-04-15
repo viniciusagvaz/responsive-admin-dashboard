8bd43799e80fbad8d25c69ceec9a87f4829ba187
Vinicius Vaz <agvazvinicius@gmail.com>
2023-04-14 21:43:57 -0300

    finished

---
+++ orders.js
@@ -0,0 +1,38 @@
+const orders =[
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with Camera Drone',
+    productNumber: '97844',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '8402',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Lozenge Drone',
+    productNumber: '00349',
+    paymentStatus: 'Paid',
+    shipping: 'In Delivered'
+  }
+]
